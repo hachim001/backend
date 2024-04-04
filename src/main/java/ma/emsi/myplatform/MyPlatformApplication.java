b4e5ef15823b6ef938e0e61b417c79a064084755
khadija abdelilah <khadija.abdelilah4@gmail.com>
2024-04-04 00:24:43 +0000

    depencies

---
+++ src/main/java/ma/emsi/myplatform/MyPlatformApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.myplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/emsi/myplatform/MyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.myplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
